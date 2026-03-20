88c4f30e38b3b1123004588dfbf06f32b2f7b87c
basecamp-labs <ffm@neg.dk>
2026-03-20 13:00:22 +0100

    Add POST route for '/ask' in httpRouter

--- convex/http.ts
+++ convex/http.ts
@@ -1,1 +1,26 @@
+import { httpRouter } from "convex/server";
 
+const http = httpRouter();
+
+http.route({
+  path: "/ask",
+  method: "POST",
+  handler: async (ctx, request) => {
+    const body = await request.json();
+    const question = body.question;
+
+    return new Response(
+      JSON.stringify({
+        answer: "Du spurgte: " + question,
+      }),
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+      }
+    );
+  },
+});
+
+export default http;
